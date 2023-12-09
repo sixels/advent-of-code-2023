633c8bbe22d29a3666943315abe8f47ea7ed561c
Sixels <sixels@protonmail.com>
2023-12-09 22:35:57 +0000

    Day 7.2 solution

--- src/solution/day07.rs
+++ src/solution/day07.rs
@@ -11,6 +11,9 @@ use super::Solution;
 const CARD_ORDER: [char; 13] = [
     'A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2',
 ];
+const CARD_ORDER_WITH_JOKER: [char; 13] = [
+    'A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J',
+];
 
 pub struct Day7;
 
@@ -69,7 +72,57 @@ impl Solution for Day7 {
     }
 
     fn solve_part_two(&self, input: &super::Input) {
-        todo!()
+        let mut hands = input
+            .lines()
+            .filter_map(|line| {
+                if !line.is_empty() {
+                    Hand::try_parse(line).ok().map(|(_, hand)| hand)
+                } else {
+                    None
+                }
+            })
+            .map(|hand| {
+                let cards = hand.cards.iter().fold(BTreeMap::new(), |mut map, &card| {
+                    let card_entry = map.entry(card).or_insert(0);
+                    *card_entry += 1;
+                    map
+                });
+
+                (hand, unsafe { Rules::matching_jokers(cards) })
+            })
+            .collect::<Vec<_>>();
+
+        hands.sort_by(|(a_hand, a_rule), (b_hand, b_rule)| {
+            match a_rule.strength().cmp(&b_rule.strength()) {
+                Ordering::Equal => a_hand
+                    .cards
+                    .iter()
+                    .zip(&b_hand.cards)
+                    .find_map(|(&a, &b)| {
+                        if a == b {
+                            return None;
+                        }
+
+                        let a_index = CARD_ORDER_WITH_JOKER.iter().position(|c| *c == a).unwrap();
+                        let b_index = CARD_ORDER_WITH_JOKER.iter().position(|c| *c == b).unwrap();
+
+                        Some(b_index.cmp(&a_index))
+                    })
+                    .unwrap(),
+                result => result,
+            }
+        });
+
+        // rules.into_iter().fold
+
+        let solution: usize = hands
+            .into_iter()
+            .enumerate()
+            // .inspect(|(i, (hand, _))| println!("{:03} *{} ({:?})", hand.bid, i + 1, hand.cards))
+            .map(|(i, (hand, _))| hand.bid * (i + 1))
+            .sum();
+
+        println!("{solution:?}");
     }
 }
 
@@ -119,6 +172,42 @@ impl Rules {
             4 if sorted[0].1 == 2 => Self::OnePair(sorted[0].0),
             5 => Self::HighCard,
             _ => unreachable_unchecked(),
+        }
+    }
+
+    unsafe fn matching_jokers(mut cards: BTreeMap<char, usize>) -> Self {
+        if let Some(&jokers) = cards.get(&'J') {
+            if let Some((_, count)) = cards
+                .iter_mut()
+                .filter(|(card, _)| **card != 'J')
+                .max_by_key(|(_, count)| **count)
+            {
+                *count += jokers;
+                cards.remove(&'J');
+            }
+        }
+
+        if cards.len() == 1 {
+            return Self::FiveOfKind(*cards.keys().next().unwrap());
+        }
+
+        let sorted = {
+            let mut cards = cards.clone().into_iter().collect::<Vec<_>>();
+            cards.sort_by_key(|(_, n)| -(*n as isize));
+            cards
+        };
+
+        match sorted.len() {
+            2 if sorted[0].1 == 4 => Self::FourOfKind(sorted[0].0),
+            2 => Self::FullHouse(sorted[0].0, sorted[1].0),
+            3 if sorted[0].1 == 3 => Self::ThreeOfKind(sorted[0].0),
+            3 => Self::TwoPair(sorted[0].0, sorted[1].0),
+            4 if sorted[0].1 == 2 => Self::OnePair(sorted[0].0),
+            5 => Self::HighCard,
+            n => {
+                println!("{n}");
+                unreachable!();
+            }
         }
     }
 
